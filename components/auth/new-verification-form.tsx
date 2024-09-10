741fc02627a40eb838c00acef8af0838d412422e
Lukiano99 <l.stojadinovic99@gmail.com>
2024-09-10 11:32:19 +0200

    feat: verification mail feature added

--- components/auth/new-verification-form.tsx
+++ components/auth/new-verification-form.tsx
@@ -1,18 +1,39 @@
 "use client";
 
+import { newVerification } from "@/actions/new-verification";
 import CardWrapper from "@/components/auth/card-wapper";
 import { LoaderIcon } from "lucide-react";
 import { useSearchParams } from "next/navigation";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
+import FormError from "@/components/form-error";
+import FormSuccess from "@/components/form-success";
 
 const NewVerificationForm = () => {
+  const [error, setError] = useState<string | undefined>();
+  const [success, setSuccess] = useState<string | undefined>();
+
   const searchParams = useSearchParams();
 
   const token = searchParams.get("token");
 
   const onSubmit = useCallback(() => {
-    console.log({ token });
-  }, [token]);
+    // for development mode, because of react strict
+    if (success || error) return;
+
+    if (!token) {
+      setError("Missing token!");
+      return;
+    }
+
+    newVerification(token)
+      .then((data) => {
+        setSuccess(data.success);
+        setError(data.error);
+      })
+      .catch(() => {
+        setError("Something went wrong!");
+      });
+  }, [token, success, error]);
 
   useEffect(() => {
     onSubmit();
@@ -25,7 +46,9 @@ const NewVerificationForm = () => {
       backButtonHref="/auth/login"
     >
       <div className="flex items-center w-full justify-center">
-        <LoaderIcon className="animate-spin" />
+        {!error && !success && <LoaderIcon className="animate-spin" />}
+        <FormSuccess message={success} />
+        {!success && <FormError message={error} />}
       </div>
     </CardWrapper>
   );
