2c17b3a846ffdc38c9e590f02ace99e66e64919c
Lukiano99 <80914501+Lukiano99@users.noreply.github.com>
2024-09-11 09:38:14 +0200

    feat: generate 2FA token

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -2,6 +2,8 @@ import { getVerificationTokenByEmail } from "@/data/verification-token";
 import { v4 as uuidv4 } from "uuid";
 import { db } from "./db";
 import { getPasswordResetTokenByEmail } from "@/data/password-reset-token";
+import crypto from "crypto";
+import { getTwoFactorTokenByEmail } from "@/data/two-factor-token";
 
 export const generatePasswordResetToken = async (email: string) => {
   const token = uuidv4();
@@ -50,4 +52,31 @@ export const generateVerificationToken = async (email: string) => {
   });
 
   return verificationToken;
+};
+
+export const generateTwoFactorToken = async (email: string) => {
+  // 6-digit number
+  const token = crypto.randomInt(100_000, 1_000_000).toString();
+
+  // TOOD: Later change to 15-minutes
+  const expires = new Date(new Date().getTime() + 3600 * 1000);
+
+  const existingToken = await getTwoFactorTokenByEmail(email);
+  if (existingToken) {
+    await db.twoFactorToken.delete({
+      where: {
+        id: existingToken.id,
+      },
+    });
+  }
+
+  const twoFactorToken = await db.twoFactorToken.create({
+    data: {
+      email,
+      token,
+      expires,
+    },
+  });
+
+  return twoFactorToken;
 };
