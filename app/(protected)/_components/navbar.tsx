a9457e58c46545b0851d30328b56a21c191d60ad
Lukiano99 <80914501+Lukiano99@users.noreply.github.com>
2024-09-14 18:57:41 +0200

    fix: little design fix for smaller screens

--- app/(protected)/_components/navbar.tsx
+++ app/(protected)/_components/navbar.tsx
@@ -7,7 +7,7 @@ import { usePathname } from "next/navigation";
 const Navbar = () => {
   const pathName = usePathname();
   return (
-    <nav className="bg-secondary flex justify-between items-center p-4 rounded-xl w-[600px] shadow-sm">
+    <nav className="bg-secondary flex justify-between items-center p-4 rounded-xl w-full md:w-[600px] shadow-sm">
       <div className="flex gap-x-2">
         <Button
           asChild
