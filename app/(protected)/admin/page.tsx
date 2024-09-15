406713a50d1d7caf181981c7e436c6b66a123fb4
Lukiano99 <80914501+Lukiano99@users.noreply.github.com>
2024-09-15 08:48:54 +0200

    feat: admin table ui init

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -1,16 +1,27 @@
-// "use client";
+"use client";
+import RoleGate from "@/components/auth/role-gate";
+import FormSuccess from "@/components/form-success";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { UserRole } from "@prisma/client";
 
-// import { useCurrentRole } from "@/hooks/use-current-role";
-import { currentRole } from "@/lib/auth";
-
-const AdminPage = async () => {
-  // in case this is a client component
-  // const role = useCurrentRole();
-
-  // in case this is a server component
-  const role = await currentRole();
-
-  return <div>current role: {role}</div>;
+const AdminPage = () => {
+  return (
+    <Card className="md:w-[600px] w-[450px]">
+      <CardHeader>
+        <p className="text-2xl text-center font-semibold">🔑 Admin</p>
+      </CardHeader>
+      <CardContent className="space-y-4">
+        <RoleGate allowedRole={UserRole.ADMIN}>
+          <FormSuccess message="You are allowed to see this content!" />
+        </RoleGate>
+        <div className="flex flex-row items-center justify-between border shadow-sm rounded-lg p-3">
+          <p className="text-sm font-medium">Admin-only Server Action</p>
+        </div>
+        <Button>Click to test</Button>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default AdminPage;
